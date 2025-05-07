0325acb44465b2145a9b674ee62d5b239c784685
Ryan Luu <ryan.luu@ryanluu.dev>
2025-05-07 15:12:53 -0700

    Automatically inject CSS
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/background.js
+++ src/background.js
@@ -1,37 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: 'OFF'
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.removeCSS({
+    files: ['style.css'],
+    target: { tabId: tab.id }
   });
 });
 
-const match = 'https://classroom.google.com';
-
-// When the user clicks on the extension action
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url && tab.url.startsWith(match)) {
-    // We retrieve the action badge to check if the extension is '' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === '' ? 'OFF' : '';
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.startsWith('https://classroom.google.com')) {
+    chrome.scripting.insertCSS({
+      files: ['style.css'],
+      target: { tabId: tabId }
     });
-
-    if (nextState === '') {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ['style.css'],
-        target: { tabId: tab.id }
-      });
-    } else if (nextState === 'OFF') {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ['style.css'],
-        target: { tabId: tab.id }
-      });
-    }
+    chrome.action.enable(tabId);
   }
 });
