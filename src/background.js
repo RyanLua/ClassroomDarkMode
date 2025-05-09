e1082a961724e3f8e1766ee3a1c302e1827ef934
Ryan Luu <ryan.luu@ryanluu.dev>
2025-05-09 11:57:52 -0700

    Cleanup code
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url && tab.url.startsWith('https://classroom.google.com')) {
+  if (tab.url?.startsWith('https://classroom.google.com')) {
     await chrome.scripting.removeCSS({
       files: ['style.css'],
       target: { tabId: tab.id }
@@ -7,12 +7,12 @@ chrome.action.onClicked.addListener(async (tab) => {
   }
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url && tab.url.startsWith('https://classroom.google.com')) {
-    chrome.scripting.insertCSS({
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (tab.url?.startsWith('https://classroom.google.com')) {
+    await chrome.scripting.insertCSS({
       files: ['style.css'],
       target: { tabId: tabId }
     });
-    chrome.action.enable(tabId);
+    await chrome.action.enable(tabId);
   }
 });
